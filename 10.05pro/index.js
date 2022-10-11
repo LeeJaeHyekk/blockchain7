e1cf91b02a90566ca779f7364a8604f62b367697
blockchain7 <ssy960105@gmail.com>
2022-10-11 20:32:12 +0900

    22.10.11
    22.10.11

---
+++ 10.05pro/index.js
@@ -0,0 +1,13 @@
+function playVideo() {
+  let playVideo = document.getElementById("itemv");
+  let playimgoff = document.getElementById("imgBt");
+
+  playVideo.play();
+  playimgoff.style.display = "none";
+
+  //   let;
+  //   play() 매서드 쓰고 , (video, audio) 재생
+  // pause() 메서드 : (video, audio) 멈추기 (= 일시정지).
+
+  //   playimg.style.playVideo = "autoplay";
+}
