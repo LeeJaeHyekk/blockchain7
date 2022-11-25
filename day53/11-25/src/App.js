96ae4270e5258674ca159d95a13848162211aa3d
LeeJaeHyekk <111038333+LeeJaeHyekk@users.noreply.github.com>
2022-11-25 12:28:12 +0900

    11-25
    11-25

--- day53/11-25/src/App.js
+++ day53/11-25/src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
 import PropsTest from "./components/PropsTest";
 import ContextTest from "./components/Contesttext";
-
+import Reducerter from "./components/Reducter";
+import Office from "./components/Office";
 function App() {
   return (
     <div className="App">
-      <PropsTest />
-      <ContextTest />
+      <PropsTest></PropsTest>
+      <ContextTest></ContextTest>
+      <Reducerter>
+        <ContextTest />
+        <Office />
+      </Reducerter>
     </div>
   );
 }
