f0277b91bd464a65aa8a476312b1c44f381b5226
LeeJaeHyekk <111038333+LeeJaeHyekk@users.noreply.github.com>
2023-05-22 09:53:31 +0900

    22.05.22 EDIT
    22.05.22 Edit

---
+++ codeTest/test.js
@@ -0,0 +1,10 @@
+function solution(list, overlist, s) {
+  let answer = "";
+  //   list.split(s);
+  //   .replace(list, overlist);
+  console.log(list.split(s));
+
+  //   return answer;
+}
+
+solution("He11oWor1d", "lloWorl", 2);
